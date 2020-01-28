b626defe3438f418b6aa79bc1991541ee096fb9b
yashpatel9900 <yashpatel.yp59@gmail.com>
2020-01-28 11:45:30 +0530

    changed style of category card

--- src/Components/category/category-card.js
+++ src/Components/category/category-card.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const CategoryCard = ({ title }) => {
     return (
-        <div className="card">
+        <div className="card category-card">
             <div className="card-body">
                 {title}
             </div>
