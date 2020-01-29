07d6538666cf0d018b9a66fa3abc2cd02a398af3
yashpatel9900 <yashpatel.yp59@gmail.com>
2020-01-29 15:32:15 +0530

    change

--- src/containers/category/category-container.js
+++ src/containers/category/category-container.js
@@ -6,7 +6,7 @@ import { list } from '../collection/collection-container'
 class Category extends React.Component {
     render() {
         return (
-            <div className="">
+            <div className="p-8 align">
                 <Custominput />
                 {
                     list.map(item => <CategoryCard key={item.title} {...item}/>)
